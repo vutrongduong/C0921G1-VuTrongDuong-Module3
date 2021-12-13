c47c0167ede8779af26a7450f5de3e41d366630d
vutrongduong <duongvu.050898@gmail.com>
2021-12-13 17:12:40 +0700

    upbai

--- case_study_database/sql_co_ban.sql
+++ case_study_database/sql_co_ban.sql
@@ -229,8 +229,26 @@ FROM
         JOIN
     hop_dong_chi_tiet hdct ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
 GROUP BY ma_dich_vu_di_kem 
-having sum(so_luong)>= all(select max(so_luong) from hop_dong_chi_tiet)
-    
+having sum(so_luong)>= (select max(so_luong) from hop_dong_chi_tiet);
+-- 14.Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung 
+-- (được tính dựa trên việc count các ma_dich_vu_di_kem)
+select hd.ma_hop_dong,ten_loai_dich_vu,ten_dich_vu_di_kem,count(dvdk.ma_dich_vu_di_kem)so_lan_su_dung
+from hop_dong hd
+join hop_dong_chi_tiet hdct on hd.ma_hop_dong=hdct.ma_hop_dong
+join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem=dvdk.ma_dich_vu_di_kem
+join dich_vu dv on hd.ma_dich_vu=dv.ma_dich_vu
+join loai_dich_vu ldv on ldv.ma_loai_dich_vu=dv.ma_loai_dich_vu
+group by ten_dich_vu_di_kem
+having count(dvdk.ma_dich_vu_di_kem)=1
+order by ma_hop_dong;
+-- 15.Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan,
+-- so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021
+
+
+
+
+
 	
 
                 
